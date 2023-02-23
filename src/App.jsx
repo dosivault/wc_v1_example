6778d3641403143dc30c633c924e60bf85f08fc6
Alan Goo <alan.goo@linecorp.com>
2023-02-23 16:40:41 +0900

    remove react logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import WalletConnect from "@walletconnect/client";
 import {SigningFinschiaClient} from "@lbmjs/finschia";
