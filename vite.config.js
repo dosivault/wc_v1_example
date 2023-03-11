f041c85ec42921059d3bbf2766e50b3b5205a28d
Alan Goo <alan.goo@linecorp.com>
2023-03-12 06:01:35 +0900

    disable minify

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false
+  },
   optimizeDeps: {
     esbuildOptions: {
         // Node.js global to browser globalThis
