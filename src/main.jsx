ae1249995fc612f450f5bbbffb8942940356eccf
Alan Goo <alan.goo@linecorp.com>
2023-03-12 12:42:20 +0900

    use vite-plugin-node-polyfills

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { Buffer } from 'buffer';
-
-window.Buffer = window.Buffer || Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
